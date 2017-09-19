d80e7b3badde7e203886769885402573823a6fd7
QuinTlapek <31743847+QuinTlapek@users.noreply.github.com>
2017-09-19 14:32:20 -0500

    Add files via upload

---
+++ Nest.py
@@ -0,0 +1,6 @@
+#Getting the rainfall for a period of years
+num_years = int(input("How many years would you like to calculate rainfall?: "))
+for years in range(num_rainfall):
+    total = 0
+    for rainfall in range(1,13):
+    current_rainfall = int(input( "How many inches did it rain?"))
